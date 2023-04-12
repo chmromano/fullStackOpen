444d877526595fc0750dc17deae9ce19e1e84b0b
Christopher Romano <christopher@romano.im>
2023-04-12 16:07:33 +0300

    Part 4: finished 4.13, 4.14 (Issue #9)

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -14,4 +14,30 @@ blogsRouter.post("/", async (request, response) => {
   response.status(201).json(savedBlog);
 });
 
+blogsRouter.delete("/:id", async (request, response) => {
+  const deletedBlog = await Blog.findByIdAndRemove(request.params.id);
+
+  deletedBlog ? response.status(204).end() : response.status(404).end();
+});
+
+blogsRouter.patch("/:id", async (request, response) => {
+  const { title, author, url, likes } = request.body;
+
+  const blog = {
+    title,
+    author,
+    url,
+    likes,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+    runValidators: true,
+  });
+
+  updatedBlog
+    ? response.status(200).json(updatedBlog)
+    : response.status(400).json({ error: "resource does not exist" });
+});
+
 module.exports = blogsRouter;
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -12,91 +12,225 @@ beforeEach(async () => {
   await Blog.insertMany(testHelper.listWithMultipleBlogs);
 });
 
-test("all blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("GET", () => {
+  test("all blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
+    const blogList = response.body;
+
+    expect(blogList).toHaveLength(testHelper.listWithMultipleBlogs.length);
+  });
 });
 
-test("all blogs are returned", async () => {
-  const response = await api.get("/api/blogs");
-  const blogList = response.body;
+describe("POST", () => {
+  test("a blog can be added to the database", async () => {
+    const postedBlog = {
+      title: "Test blog",
+      author: "Test Testerson",
+      url: "http://www.testblog.com/this-is-a-test-blog",
+      likes: 3,
+    };
 
-  expect(blogList).toHaveLength(testHelper.listWithMultipleBlogs.length);
+    await api
+      .post("/api/blogs")
+      .send(postedBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await testHelper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(
+      testHelper.listWithMultipleBlogs.length + 1
+    );
+    expect(blogsAtEnd).toEqual(
+      expect.arrayContaining([expect.objectContaining(postedBlog)])
+    );
+  });
 });
 
-test("blogs have the 'id' propery", async () => {
-  const response = await api.get("/api/blogs");
-  const blogList = response.body;
+describe("DELETE", () => {
+  test("deletion of a blog succeeds with status code 204 if id is valid", async () => {
+    const blogsAtStart = await testHelper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
 
-  blogList.forEach((blog) => expect(blog.id).toBeDefined());
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtEnd = await testHelper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(
+      testHelper.listWithMultipleBlogs.length - 1
+    );
+    expect(blogsAtEnd).not.toContainEqual(
+      expect.objectContaining(blogToDelete)
+    );
+  });
+
+  test("deletion of blog fails with status code 404 if blog does not exist", async () => {
+    const blogsAtStart = await testHelper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtHalf = await testHelper.blogsInDb();
+
+    expect(blogsAtHalf).toHaveLength(
+      testHelper.listWithMultipleBlogs.length - 1
+    );
+    expect(blogsAtHalf).not.toContainEqual(
+      expect.objectContaining(blogToDelete)
+    );
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(404);
+
+    const blogsAtEnd = await testHelper.blogsInDb();
+
+    expect(blogsAtEnd).toEqual(blogsAtHalf);
+  });
 });
 
-test("a blog can be added to the database", async () => {
-  const postedBlog = {
-    title: "Test blog",
-    author: "Test Testerson",
-    url: "http://www.testblog.com/this-is-a-test-blog",
-    likes: 3,
-  };
+describe("PATCH", () => {
+  test("update of a blog succeeds with status code 200 if id is valid", async () => {
+    const blogsAtStart = await testHelper.blogsInDb();
+    const blogToUpdate = blogsAtStart[0];
 
-  await api
-    .post("/api/blogs")
-    .send(postedBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    const updatedBlog = {
+      title: "Testing patch",
+      author: "Test Testerson",
+      url: "https://test.com",
+      likes: 2,
+    };
 
-  const blogsAtEnd = await testHelper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(testHelper.listWithMultipleBlogs.length + 1);
-  expect(blogsAtEnd).toEqual(
-    expect.arrayContaining([expect.objectContaining(postedBlog)])
-  );
+    await api
+      .patch(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedBlog)
+      .expect(200);
+
+    updatedBlog.id = blogToUpdate.id;
+
+    const blogsAtEnd = await testHelper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(testHelper.listWithMultipleBlogs.length);
+    expect(blogsAtEnd).toContainEqual(expect.objectContaining(updatedBlog));
+  });
+
+  test("update of a blog succeeds with status code 200 even if object parameters are undefined", async () => {
+    const blogsAtStart = await testHelper.blogsInDb();
+    const blogToUpdate = blogsAtStart[0];
+
+    const updatedBlog = {
+      title: "Testing patch with undefined parameters",
+    };
+
+    await api
+      .patch(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedBlog)
+      .expect(200);
+
+    updatedBlog.id = blogToUpdate.id;
+    updatedBlog.author = blogsAtStart[0].author;
+    updatedBlog.url = blogsAtStart[0].url;
+    updatedBlog.likes = blogsAtStart[0].likes;
+
+    const blogsAtEnd = await testHelper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(testHelper.listWithMultipleBlogs.length);
+    expect(blogsAtEnd).toContainEqual(expect.objectContaining(updatedBlog));
+  });
+
+  test("update of a blog fails with status code 400 if blog does not exist", async () => {
+    const blogsAtStart = await testHelper.blogsInDb();
+    const blogToUpdate = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToUpdate.id}`).expect(204);
+
+    const updatedBlog = {
+      title: "Testing patch",
+      author: "Test Testerson",
+      url: "https://test.com",
+      likes: 2,
+    };
+
+    await api
+      .patch(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedBlog)
+      .expect(400);
+
+    updatedBlog.id = blogToUpdate.id;
+
+    const blogsAtEnd = await testHelper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(
+      testHelper.listWithMultipleBlogs.length - 1
+    );
+    expect(blogsAtEnd).not.toContainEqual(expect.objectContaining(updatedBlog));
+  });
 });
 
-test("if likes are missing default to 0", async () => {
-  const postedBlog = {
-    title: "Test blog",
-    author: "Test Testerson",
-    url: "http://www.testblog.com/this-is-a-test-blog",
-  };
+describe("validate", () => {
+  test("blogs have the 'id' propery", async () => {
+    const response = await api.get("/api/blogs");
+    const blogList = response.body;
 
-  const expectedBlog = {
-    title: "Test blog",
-    author: "Test Testerson",
-    url: "http://www.testblog.com/this-is-a-test-blog",
-    likes: 0,
-  };
+    blogList.forEach((blog) => expect(blog.id).toBeDefined());
+  });
 
-  await api
-    .post("/api/blogs")
-    .send(postedBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+  test("if likes are missing default to 0", async () => {
+    const postedBlog = {
+      title: "Test blog",
+      author: "Test Testerson",
+      url: "http://www.testblog.com/this-is-a-test-blog",
+    };
 
-  const blogsAtEnd = await testHelper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(testHelper.listWithMultipleBlogs.length + 1);
-  expect(blogsAtEnd).toEqual(
-    expect.arrayContaining([expect.objectContaining(expectedBlog)])
-  );
-});
+    const expectedBlog = {
+      title: "Test blog",
+      author: "Test Testerson",
+      url: "http://www.testblog.com/this-is-a-test-blog",
+      likes: 0,
+    };
 
-test("if title is missing respond with 400 bad request", async () => {
-  const noTitleBlog = {
-    author: "Test Testerson",
-    url: "http://www.testblog.com/this-is-a-test-blog",
-  };
+    await api
+      .post("/api/blogs")
+      .send(postedBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  await api.post("/api/blogs").send(noTitleBlog).expect(400);
-});
+    const blogsAtEnd = await testHelper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(
+      testHelper.listWithMultipleBlogs.length + 1
+    );
+    expect(blogsAtEnd).toEqual(
+      expect.arrayContaining([expect.objectContaining(expectedBlog)])
+    );
+  });
 
-test("if url is missing respond with 400 bad request", async () => {
-  const noUrlBlog = {
-    title: "Test blog",
-    author: "Test Testerson",
-  };
+  test("if title is missing respond with 400 bad request", async () => {
+    const noTitleBlog = {
+      author: "Test Testerson",
+      url: "http://www.testblog.com/this-is-a-test-blog",
+    };
 
-  await api.post("/api/blogs").send(noUrlBlog).expect(400);
+    await api.post("/api/blogs").send(noTitleBlog).expect(400);
+
+    const blogsAtEnd = await testHelper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(testHelper.listWithMultipleBlogs.length);
+  });
+
+  test("if url is missing respond with 400 bad request", async () => {
+    const noUrlBlog = {
+      title: "Test blog",
+      author: "Test Testerson",
+    };
+
+    await api.post("/api/blogs").send(noUrlBlog).expect(400);
+
+    const blogsAtEnd = await testHelper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(testHelper.listWithMultipleBlogs.length);
+  });
 });
 
 afterAll(async () => {
