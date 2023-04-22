1eed650deaa754a3b77a67c6fec0667d10bd83aa
Christopher Romano <christopher@romano.im>
2023-04-22 22:06:03 +0300

    Part 6: Finished 6.22. (Issue #14)

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
-import { useQuery } from "react-query";
-import { getAnecdotes } from "./requests";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { getAnecdotes, updateAnecdote } from "./requests";
 
 const App = () => {
+  const queryClient = useQueryClient();
+
   const result = useQuery("anecdotes", getAnecdotes);
+
+  const anecdotes = result.data;
+
+  const updateAnecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("anecdotes");
+    },
+  });
+
+  const handleVote = (anecdote) => {
+    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
+  };
 
   if (result.isError) {
     return <div>There was a problem communicating with the server.</div>;
@@ -15,12 +29,6 @@ const App = () => {
     return <div>Loading data...</div>;
   }
 
-  const anecdotes = result.data;
-
-  const handleVote = (anecdote) => {
-    console.log("vote");
-  };
-
   return (
     <div>
       <h3>Anecdote app</h3>
@@ -28,15 +36,17 @@ const App = () => {
       <Notification />
       <AnecdoteForm />
 
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
+      {anecdotes
+        .sort((a, b) => b.votes - a.votes)
+        .map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => handleVote(anecdote)}>vote</button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
