96dbd16214a9e96510bbdd5003628b0b7f46276b
Christopher Romano <christopher@romano.im>
2023-05-18 19:13:30 +0300

    Part 7: Added webpack configuration for extending-the-bloglist exercise. (Issue #16)

--- part7/extending-the-bloglist/src/index.jsx
+++ part7/extending-the-bloglist/src/index.jsx
@@ -1,8 +1,12 @@
+import "core-js/stable/index.js";
+import "regenerator-runtime/runtime.js";
+
 import App from "./App";
 import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import store from "./store";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
---
+++ part7/extending-the-bloglist/webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+
+const config = (env, argv) => {
+  console.log(env, argv);
+
+  const backend_url =
+    argv.mode === "production"
+      ? "https://notes2023.fly.dev/api/notes"
+      : "http://localhost:3001/notes";
+
+  return {
+    entry: "./src/index.jsx",
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "main.js",
+    },
+    devServer: {
+      static: path.resolve(__dirname, "build"),
+      compress: true,
+      port: 3000,
+    },
+    devtool: "source-map",
+    plugins: [
+      new HtmlWebpackPlugin({ template: "./public/index.html" }),
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url),
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(jsx|js)$/,
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ["", ".js", ".jsx"],
+    },
+  };
+};
+
+module.exports = config;
